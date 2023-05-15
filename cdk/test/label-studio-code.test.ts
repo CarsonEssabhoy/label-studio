5025eb3868911c0413fd07b824cd290e61ddf36a
Augene Pak <ajp824@uw.edu>
2023-05-15 12:28:02 -0700

    Initial CDK setup

---
+++ cdk/test/label-studio-code.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LabelStudioCode from '../lib/label-studio-code-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/label-studio-code-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LabelStudioCode.LabelStudioCodeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
